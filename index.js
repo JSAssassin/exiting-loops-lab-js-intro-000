a2fbfb9f3afaa1f90e9af98c754709fa9b2587e0
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-30 14:09:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for(let i = 0; i < array.length; i++) {
-
+    let element = array[i];
+    if(findFn(element)){
+      return element;
     }
   }
 
