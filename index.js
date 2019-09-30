cd9803583e3380b1199d173f04d7f80b7094dc47
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-30 13:40:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ function breakOut(array, changeValue, stopValue){
   array.forEach((element)=> {
     if(element === stopValue) {
       break;
-    } else {
-      element = changeValue;
     }
+
+    element = changeValue;
+
   })
 }
